3c1f75fe86a32adddd7621b2255c4acc102c731c
geunhye-kim <mykgh6514@naver.com>
2023-04-26 16:39:05 +0900

    fixed createsuperuser

--- users/models.py
+++ users/models.py
@@ -3,23 +3,23 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
-            email=self.normalize_email(email),
+            email=self.normalize_email(email), **extra_fields
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, password, **extra_fields):
         user = self.create_user(
-            email,
-            password=password,
+            email, password, **extra_fields
         )
+
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -47,7 +47,7 @@ class User(AbstractBaseUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['name', 'age', 'gender', 'introduction',]
 
     def __str__(self):
         return self.email
